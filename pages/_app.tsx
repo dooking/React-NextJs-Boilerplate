cd464748a10491add8d9e872bed411046da5e462
dooking <123456ghghgh@naver.com>
2022-05-03 14:49:56 +0900

    feat: style 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,22 @@
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from 'styles/globalStyles';
+import { theme } from '../styles/theme';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <title>boilerplate</title>
+      </Head>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default MyApp;
--- styles/globalStyles.ts
+++ styles/globalStyles.ts
@@ -1,38 +1,54 @@
-import reset from 'styled-reset';
 import { createGlobalStyle } from 'styled-components';
+import { reset } from 'styled-reset';
+import { media } from './theme';
 
-const GlobalStyle = createGlobalStyle`
-  ${reset}
-  * {
-    box-sizing: border-box;
-  }
-  body{
-    font-family: -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif;
-  }
-  a {
-    color: inherit;
-    text-decoration: none;
-  }
-  input, button {
-    background-color: transparent;
-    border: none;
-    outline: none;
-  }
-  h1, h2, h3, h4, h5, h6{
-    font-family:'Maven Pro', sans-serif;
-  }
-
-  @media only screen and (max-width: 768px) {
-    body {
-      font-size: 12px;
+export const GlobalStyle = createGlobalStyle`
+    ${reset}
+    :focus {
+        outline: none;
+        border: none;
     }
-  }
-
-  @media only screen and (max-width: 576px) {
-    body {
-      font-size: 10px;
+    ::-webkit-scrollbar {
+        display: none;
     }
-  }
+    html{
+        font-size: 11px;
+        -webkit-text-size-adjust: none;
+        font-family: -apple-system,BlinkMacSystemFont,helvetica,Apple SD Gothic Neo,sans-serif;       
+        font-display: fallback;
+        ${media.tablet}{
+            font-size: 10px;
+        }
+        -ms-overflow-style: none;
+        scrollbar-width: none;
+    }
+    button {
+        background: none;
+        padding: 0;
+        border: none;
+        cursor: pointer;
+        &:disabled {
+            cursor: default;
+            fill: #f2f3f4;
+        }
+    }
+
+    .pc-tablet-only {
+        display: block;
+        ${media.mobile} {
+            display: none;
+        }
+    }
+    .tablet-mobile-only{
+        display: none;
+        ${media.tablet}{
+            display:block;
+        }
+    }
+    .mobile-only {
+        display: none;
+        ${media.mobile} {
+            display: block;
+        }
+    }
 `;
-
-export default GlobalStyle;
